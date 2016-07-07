67c2082fe815c1bf00e50e846bc7c0170d3dd7e2
Laert Pires <Laert Pires>
2016-07-07 12:17:10 +1000

    Updating index.php

--- www/index.php
+++ www/index.php
@@ -1,19 +1,16 @@
 
-
-
 <?php
 // Connecting, selecting database
 
 echo "<b> AON Demo TEST </b>\n";
 echo "</br>\n";
 
-$link = mysql_connect('localhost', 'root', 'root')
-    or die('Could not connect: ' . mysql_error());
+$link = mysql_connect('localhost', 'root', 'root') or die('Could not connect: ' . mysql_error());
 echo 'Connected successfully';
-mysql_select_db('my_database') or die('Could not select database');
+mysql_select_db('mysql') or die('Could not select database');
 
 // Performing SQL query
-$query = 'SELECT * FROM USERS';
+$query = 'SELECT * FROM USER';
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 // Printing results in HTML
